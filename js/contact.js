f4ad3a44d1f0fad05c3a792510a88fd6c06d27af
Dantr3b <94704718+Dantr3b@users.noreply.github.com>
2023-11-15 16:04:19 +0100

    creation page contact

---
+++ js/contact.js
@@ -0,0 +1,13 @@
+function updateEmailLink() {
+    var subject = document.getElementById("SubjetInput").value;
+    var name = document.getElementById("NameInput").value;
+    var body = document.getElementById("bodyInput").value;
+
+    var emailBody = body + "\n\n" +
+                    "Cordialement " + name;
+
+    var emailLink = document.getElementById("emailLink");
+    emailLink.href = "mailto:someone@example.com" +
+                     "?subject=" + encodeURIComponent(subject) +
+                     "&body=" + encodeURIComponent(emailBody);
+}
